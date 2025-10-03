0f53f2dfb06b91f2d0df161797ec3bf14214053f
LauMoser <laura.moser@germinare.org.br>
2025-10-03 07:59:24 -0300

    desafio final

--- 02_Fundamentos/src/App.jsx
+++ 02_Fundamentos/src/App.jsx
@@ -2,19 +2,24 @@
 import './App.css'
 import FirstComponent from './components/FirstComponent'
 import TemplateExpressions from './components/TemplateExpressions'
-import MyComponent from './components/MyComponent' 
+import MyComponent from './components/MyComponent'
 import Events from './components/Events'
-import Challenge from './components/Challenge' 
+import Challenge from './components/Challenge'
+import FinalChallenge from './components/FinalChallenge'
 
 function App() {
   return (
     <div className="App">
       <h1>Fundamentos React</h1>
-      <FirstComponent/>
-      <TemplateExpressions/>
-      <MyComponent/>
-      <Events/>
-      <Challenge/>
+      <FirstComponent />
+      <TemplateExpressions />
+      <MyComponent />
+      <MyComponent />
+      <Events />
+      <Challenge />
+      <FinalChallenge x={5} y={7} /> 
+      <FinalChallenge x={10} y={20} />
+      <FinalChallenge x={3} y={9} />   
     </div>
   )
 }
